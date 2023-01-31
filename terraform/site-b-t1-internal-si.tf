2a7792478c5d3431241b7287d41a261a7d6a1382
livefire <nnikodimov@vmware.com>
2023-01-31 10:05:02 +0000

    terraform script update

---
+++ terraform/site-b-t1-internal-si.tf
@@ -0,0 +1,26 @@
+data "nsxt_policy_transport_zone" "host-vlan-tz" {
+  provider = nsxt.lm-site-b
+  display_name = "host-vlan-tz"
+}
+
+data "nsxt_policy_tier1_gateway" "t1-internal" {
+  provider = nsxt.lm-site-b
+  display_name = "t1-internal"
+}
+
+resource "nsxt_policy_vlan_segment" "vlan-222" {
+  provider = nsxt.lm-site-b
+  display_name = "vlan-222"
+  vlan_ids     = [222]
+  transport_zone_path = data.nsxt_policy_transport_zone.host-vlan-tz.path
+}
+
+resource "nsxt_policy_tier1_gateway_interface" "si-vlan-222" {
+  provider = nsxt.lm-site-b
+  display_name           = "si-vlan-222"
+  description            = "connection to vlan-222"
+  gateway_path           = data.nsxt_policy_tier1_gateway.t1-internal.path
+  segment_path           = nsxt_policy_vlan_segment.vlan-222.path
+  subnets                = ["192.168.222.1/24"]
+  mtu                    = 1500
+}
